6b90701a3b7dc2f20722c29809acf55811b47b52
Fredrik Wendt <fredrik@wendt.se>
2014-10-29 01:06:01 +0100

    scripts for push/pull of docker image to/from docker-registry

---
+++ pull-docker-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$GO_PIPELINE_LABEL" ] ; then
+    # probably manual testing, lets get a random enough number
+    GO_PIPELINE_LABEL=`mktemp -u | cut -d '.' -f 2 | tr [:upper:] [:lower:]`
+    echo $GO_PIPELINE_LABEL
+fi
+
+if [ -z "DOCKER_REGISTRY_HOST" ] ; then
+    DOCKER_REGISTRY_HOST=docker-registry
+fi
+
+docker pull $DOCKER_REGISTRY_HOST:5000/doodleshop/$GO_PIPELINE_LABEL
